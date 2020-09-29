487b41f0efa37e97b2a2aa48f3c3c3f82daa377d
Niall James <niall.james@dvla.gov.uk>
2020-09-29 11:03:49 +0100

    adding bootstrap

--- react-app-template/src/App.js
+++ react-app-template/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// This is a react component (custom html element)
+// It is the apps root component
+// all other components eill be rendered within it
 export default function App() {
   return (
-    <h1>React App Template</h1>
+    <div className="container">
+      <h1 className="display-4 mt-4 mb-4">React App Template</h1>
+      <hr />
+    </div>
+
   );
 }
 
